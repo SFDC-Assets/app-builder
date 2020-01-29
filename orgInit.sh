9527b5ca3b58d1f1dcd3637101e40fc1a21969c7
Gabe Sumner <gsumner@gsumner-ltm8.internal.salesforce.com>
2020-01-28 16:24:17 -0800

    Added Vehicle object

--- orgInit.sh
+++ orgInit.sh
@@ -1,3 +1,5 @@
-sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --userprefix admin -o appbuilder.demo
+sfdx shane:org:create -f config/project-scratch-def.json -d 3 -s --wait 60 --userprefix admin -o appbuilder.demo
 sfdx force:source:push
+sfdx force:user:permset:assign -n AppBuilder
 sfdx shane:user:password:set -g User -l User -p salesforce1
+sfdx force:org:open
