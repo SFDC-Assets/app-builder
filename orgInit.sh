5142e01b24b47839df9d5a1f9c6b295d10552c9a
Gabe Sumner <gsumner@gsumner-ltm8.internal.salesforce.com>
2020-03-10 15:28:17 -0700

    Switching to standard org:create

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,5 @@
-sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --userprefix admin -o appbuilder.demo
+#sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --userprefix admin -o appbuilder.demo
+sfdx force:org:create -f config/project-scratch-def.json -s -d 30 -w 60
 sfdx force:source:push
 sfdx force:user:permset:assign -n AppBuilder
 sfdx shane:user:password:set -g User -l User -p salesforce1
