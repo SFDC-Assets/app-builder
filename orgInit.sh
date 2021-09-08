f7b72e40918fdf4b973455de9748998d19a3a709
Jonathan Zhou <34146693+zhou059@users.noreply.github.com>
2021-09-08 10:51:01 -0700

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,5 @@
 #sfdx shane:org:create -f config/project-scratch-def.json -d 3 -s --wait 60 --userprefix admin -o appbuilder.demo
-sfdx force:org:create -f config/project-scratch-def.json -s -d 30 -w 60
+sfdx force:org:create -f config/project-scratch-def.json -s -d 3 -w 60
 sfdx force:source:push
 sfdx force:user:permset:assign -n AppBuilder
 sfdx shane:user:password:set -g User -l User -p salesforce1
