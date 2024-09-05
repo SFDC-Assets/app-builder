cf23ab0490c6bab18b47ae52186473593391df01
zhou059 <zhou059@gmail.com>
2024-09-05 12:20:03 -0700

    init

--- orgInit.sh
+++ orgInit.sh
@@ -1,7 +1,7 @@
 #sfdx shane:org:create -f config/project-scratch-def.json -d 3 -s --wait 60 --userprefix admin -o appbuilder.demo
-sfdx force:org:create -f config/project-scratch-def.json -s -d 3 -w 60
-sfdx force:source:push
-sfdx force:user:permset:assign -n AppBuilder
-sfdx shane:user:password:set -g User -l User -p salesforce1
-sfdx force:org:open
-sfdx automig:load -d data
+sf org create scratch -f config/project-scratch-def.json -s -d 3 -w 60
+sf project deploy start
+sf org assign permset -n AppBuilder
+sf shane user password set -g User -l User -p salesforce1
+sf org open
+sf automig load -d data
