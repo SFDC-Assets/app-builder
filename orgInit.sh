9fc9815aa3b24a123c1de6cc51d215c6f56b3fe3
Sumanth Ghanta <92057458+sghanta-sf@users.noreply.github.com>
2024-10-23 12:20:58 -0700

    Update orgInit.sh for cli changes

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,4 @@
-sf demoutil org create scratch -f config/project-scratch-def.json -d 3 -s
+sf demoutil org create scratch -p admin -e appbuilder.demo -d 20 -w 10 -f config/project-scratch-def.json -s
 
 sf project deploy start
 sf org assign permset -n AppBuilder
